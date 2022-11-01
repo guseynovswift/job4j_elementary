4be3857be50c5929b38d1e274500bb4d009de8d5
guseynov <guseynov.smm@gmail.com>
2022-11-01 15:56:39 +0500

    Four Lesson (Calculator)

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,7 +2,12 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("хейййй");
+        int age = 18;
+        double length = 78.0332;
+        boolean baby = true;
+        System.out.println(age);
+        System.out.println(length);
+        System.out.println(baby);
     }
 }
 
