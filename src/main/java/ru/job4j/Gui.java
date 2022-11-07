5d0b78263a7e69e57cda65b0a1edd4b5fe374447
guseynov <guseynov.smm@gmail.com>
2022-11-07 12:40:13 +0500

    6 Lesson (Starting Bro Code)

---
+++ src/main/java/ru/job4j/Gui.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+import javax.swing.JOptionPane;
+
+public class Gui {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name");
+    }
+}
---
+++ src/main/java/ru/job4j/Scanner1.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello "+name);
+    }
+}
